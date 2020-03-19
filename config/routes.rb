033d6468ae43191a8b77b9e17e0f3d988b23ecb3
Thibaud Allain <thibaudallainta@gmail.com>
2020-03-19 14:38:12 +0100

    it is possible to apply for a game

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :games, only: [:index, :new, :create, :show]
+  resources :games, only: [:index, :new, :create, :show] do
+    resources :game_teams, only: [:create, :edit]
+  end
 end
