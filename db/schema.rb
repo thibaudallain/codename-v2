83dab8aeb7a4a687fc53ab1a612a8e1fd0164d0a
Thibaud Allain <thibaudallainta@gmail.com>
2020-03-19 09:11:36 +0100

    added status and name to Game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_151446) do
+ActiveRecord::Schema.define(version: 2020_03_19_080838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_03_18_151446) do
     t.datetime "updated_at", null: false
     t.integer "red"
     t.integer "blue"
+    t.string "status"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
