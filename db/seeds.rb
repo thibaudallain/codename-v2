eed889ae68a32975adeb58dfdfa45a3df9185dac
Thibaud Allain <thibaudallainta@gmail.com>
2020-03-19 09:04:12 +0100

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,20 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+
+require 'csv'
+
+Word.destroy_all
+
+csv_options = { col_sep: ';', quote_char: '"', headers: :first_row }
+filepath = "db/words.csv"
+
+CSV.foreach(filepath, csv_options) do |row|
+  Word.create(word: row[0])
+end
+
+User.create( email: 't@code.com', password: "abc123" )
+User.create( email: 'a@code.com', password: "abc123" )
+User.create( email: 'b@code.com', password: "abc123" )
+User.create( email: 'c@code.com', password: "abc123" )
