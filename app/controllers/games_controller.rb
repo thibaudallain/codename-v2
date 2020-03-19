9c425d28599f4539d379bc5bcb40533fe101d856
Thibaud Allain <thibaudallainta@gmail.com>
2020-03-19 11:40:17 +0100

    game creation ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,22 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.status = "Pending"
+    @game.user = current_user
+    @game.save
+    redirect_to games_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name)
+  end
 end
