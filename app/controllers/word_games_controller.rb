21c90df5a9bdc88cfa6aade2326f018b783fb9ea
Thibaud Allain <thibaudallainta@gmail.com>
2020-03-20 13:10:57 +0100

    you can click only if turn of your team

---
+++ app/controllers/word_games_controller.rb
@@ -0,0 +1,5 @@
+class WordGamesController < ApplicationController
+  def update
+    raise
+  end
+end
---
+++ test/controllers/word_games_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WordGamesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
