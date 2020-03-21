d4f8696cae455e93533bd6225bc3a86e2cca5cb8
Thibaud Allain <thibaudallainta@gmail.com>
2020-03-21 13:44:58 +0100

    changed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://confinement-coronavirus.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://confinement-coronavirus.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
